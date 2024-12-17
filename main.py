c38cc140eaf0a2536106aaccfe14905cca91694f
NwizuEmmanuel <nwizuemmanuel200@gmail.com>
2024-12-17 17:39:00 +0800

    added templates

--- main.py
+++ main.py
@@ -4,4 +4,23 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    render_template('form.html')
+    return render_template('length.html')
+
+@app.route("/length")
+def length():
+    return render_template('length.html')
+
+@app.route("/weight")
+def weight():
+    return render_template("weight.html")
+
+@app.route("/temperature")
+def temperature():
+    return render_template("temperature.html")
+
+@app.route("/convert")
+def convert():
+    return render_template("result.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
